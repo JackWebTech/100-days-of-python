1e5f8ab89cd2ae1d8d5423e62ecaf98f7b169218
Jackson <jacksondr2014@yahoo.com>
2025-12-11 14:27:46 -0300

    Initial commit: Starting 100 Days of Python

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city_name = input("What city are you in?\n")
+pet_name = input("What is your pet´ś name?\n")
+
+print("Your band name is " + city_name + " " + pet_name)
