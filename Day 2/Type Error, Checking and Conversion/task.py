3d13ad5b6ee317deb5212e639b447e9f33212f82
Jackson <jacksondr2014@yahoo.com>
2025-12-13 00:38:28 -0300

    Day 2: tip calculator project

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,19 @@
-len(12345)
+from operator import length_hint
+
+print(type("Jackson"))
+print(type(123))
+print(type(3.14))
+print(type(True))
+
+
+# Minha solução:
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
+# Solução da Angela (mais legível):
+# name_of_the_user  = input("Enter your name")
+# length_of_the_name = len(name_of_the_user )
+# print("Number of letters in your name: " + str(length_of_the_name))
+
+name_of_the_user = input("What is your name?")
+length_of_the_name = len(name_of_the_user)
+print("Number of letters in your name: " + str(length_of_the_name))
